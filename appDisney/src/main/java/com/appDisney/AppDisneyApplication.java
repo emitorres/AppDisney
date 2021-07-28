ec1b7a2f810eaa620d2ac7366ba9355d13690337
Emiliano Torres <emitorres93@gmail.com>
2021-07-28 17:27:50 -0300

    Creacion del proyecto

---
+++ appDisney/src/main/java/com/appDisney/AppDisneyApplication.java
@@ -0,0 +1,13 @@
+package com.appDisney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDisneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDisneyApplication.class, args);
+	}
+
+}
